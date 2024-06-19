5e184751e6629f023fe821daaef1e6ee3b5b4a50
Richard Darst <rkd@zgib.net>
2024-06-20 02:24:05 +0300

    zulip_reaction_counter: Support narrowing

--- zulip_reaction_counter.py
+++ zulip_reaction_counter.py
@@ -28,13 +28,16 @@ processed yourself.
 """
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('zuliprc', help="zuliprc file to use to connect")
-    parser.add_argument('--verbose', '-v', action='store_true', help="Be more verbose (on stderr)")
+    parser.add_argument('--narrow', default="streams:public", help='Select messages based on this narrow (search filter).  This value is split by spaces and then by the ":" symbol to send to the API.  You should include either "streams:" or "stream:" to force a search of all history, not only the history of the API user.  For searching based on search terms, you can use "search:TERM".  For zulip>9.0, you can include "has:reaction". (default "%(default)s)"')
     parser.add_argument('--chunk-size', default=1000, type=int, help="Chunk size for pagination (default %(default)s)")
+    parser.add_argument('--no-header', action='store_true', help="Don't print a CSV header")
+    parser.add_argument('--verbose', '-v', action='store_true', help="Be more verbose (on stderr)")
     args = parser.parse_args()
 
     # Initial i/o setup
     client = zulip.Client(config_file="~/zuliprc")
-    print("#stream,sender,reactor,reaction,timestamp", file=sys.stdout)
+    if not args.no_header:
+        print("#stream,sender,reactor,reaction,timestamp", file=sys.stdout)
     write = writer(sys.stdout, dialect='excel')
 
     # Cache stream info
@@ -51,17 +54,20 @@ processed yourself.
         "num_before": args.chunk_size,
         "num_after": 0,
         "include_anchor": True,
-        "narrow": [{"operator":"channels", "operand": "public"}],
+        #"narrow": [{"operator":"channels", "operand": "public"}],
+        "narrow": [
+            {"operator":x.split(':')[0], "operand": x.split(':', 1)[1]}
+            for x in args.narrow.split()],
         }
 
     # Do our pagination (until we are out)
     n_messages = 0
     while True:
         result = client.get_messages(request)
-        n_messages += len(result['messages'])
         if args.verbose:
             pprint(result, stream=sys.stderr)
             print(len(result['messages']))
+        n_messages += len(result['messages'])
         for message in reversed(result['messages']):
             sname = message['sender_full_name']
             stream_id = message['stream_id']
@@ -75,10 +81,11 @@ processed yourself.
                 write.writerow((stream_name, sname, rname, remoji, time))
 
         print("Processed %d messages so far"%n_messages, file=sys.stderr)
-        request["include_anchor"] = False  # future ones exclude the anchor
-        request["anchor"] = result['messages'][0]['id']
+
         if result['found_oldest']:
             break
+        request["include_anchor"] = False  # future ones exclude the anchor
+        request["anchor"] = result['messages'][0]['id']
         #break
 
 
